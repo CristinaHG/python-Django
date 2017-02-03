ee926bb9cafcf0a6c8416c78fa8894c6e9d1c1f2
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-02-03 13:57:42 +0100

    hello word,collections,comments and list

---
+++ python-Django/ExampleCourse/HelloWord.py
@@ -0,0 +1,25 @@
+print ("Hello World")
+
+#collections and comments
+
+#List
+fruits=['Banana','Strawberry','Apple','Grapes']
+print(fruits[0])
+print(fruits[-1])
+print(fruits[-3])
+
+#List slicing
+numbers=[0,1,2,3,4,5,6,7,8,9,10]
+from_2to7=numbers[2:7]
+print from_2to7
+geatherThan3=numbers[3:]
+print geatherThan3
+pairsNumbers=numbers[::2]
+print pairsNumbers
+notpairNumbers=numbers[1::2]
+print notpairNumbers
+
+print (4 in numbers)
+
+#dictionaries and None value
+cities={'Granada','Sevilla','Córdoba'}

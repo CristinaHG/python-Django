314c468a6673a248c4c9d91d1e409180b0bd3690
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-02-08 12:16:41 +0100

    starting Django app

---
+++ ExampleCourse/Frirk/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cris/mrcrstnherediagmez@gmail.com/Curso_pythonDjango/python-Django/ExampleCourse/Frirk/env/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

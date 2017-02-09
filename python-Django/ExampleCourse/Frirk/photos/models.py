834fd342fb4d54d8d2a93c28bd293e07af63ebfb
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-02-09 10:28:57 +0100

    adding Photo model to admin

--- python-Django/ExampleCourse/Frirk/photos/models.py
+++ python-Django/ExampleCourse/Frirk/photos/models.py
@@ -17,7 +17,8 @@ LICENSES=(
 class Photo(models.Model):
     name=models.CharField(max_length=150)
     url=models.URLField()
-    description=models.TextField(bank=True,null=True,default="")
+    description=models.TextField(blank=True,null=True,default="")
     created_at=models.DateTimeField(auto_now_add=True)
     modified_at=models.DateTimeField(auto_now=True)
     licence=models.CharField(max_length=3,choices=LICENSES)
+

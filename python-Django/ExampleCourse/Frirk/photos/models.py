4d94c2b078e619c5b51ff48620d4b1f250315b6d
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-02-10 10:26:48 +0100

    filtering photos by viisibility

--- python-Django/ExampleCourse/Frirk/photos/models.py
+++ python-Django/ExampleCourse/Frirk/photos/models.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -12,15 +15,25 @@ LICENSES=(
     (CREATIVE_COMMONS,'Creative Commons')
 )
 
+PUBLIC='PUB'
+PRIVATE='PRI'
+
+VISIBILITY=(
+    (PUBLIC,'Pública'),
+    (PRIVATE,'Privada')
+)
+
 
 # Create your models here.
 class Photo(models.Model):
+    owner=models.ForeignKey(User)
     name=models.CharField(max_length=150)
     url=models.URLField()
     description=models.TextField(blank=True,null=True,default="")
     created_at=models.DateTimeField(auto_now_add=True)
     modified_at=models.DateTimeField(auto_now=True)
     licence=models.CharField(max_length=3,choices=LICENSES)
+    visibility=models.CharField(max_length=3,choices=VISIBILITY,default=PUBLIC)
 
     def __unicode__(self): #no params method
         return self.name
--- python-Django/ExampleCourse/Frirk/photos/views.py
+++ python-Django/ExampleCourse/Frirk/photos/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from photos.models import Photo
+from photos.models import Photo,PUBLIC
 
 
 def home(request):
-    photos=Photo.objects.all().order_by('-created_at')
+    photos=Photo.objects.filter(visibility=PUBLIC).order_by('-created_at')
     # html='<ul>'
     # for photo in photos:
     #     html+='<li>'+photo.name+'</li>'

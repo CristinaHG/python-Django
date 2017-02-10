6bef2ce72aaa30cb3bad6faab1fd0d9174fa85c8
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-02-10 13:29:28 +0100

    adding home url

--- python-Django/ExampleCourse/Frirk/frikr/urls.py
+++ python-Django/ExampleCourse/Frirk/frikr/urls.py
@@ -19,6 +19,6 @@ import photos
 from photos import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', photos.views.home),
-    url(r'^photos/(?P<pk>[0-9]+)$',photos.views.detail),
+    url(r'^$', photos.views.home,name='photos_home'),
+    url(r'^photos/(?P<pk>[0-9]+)$',photos.views.detail,name='photo_detail'),
 ]

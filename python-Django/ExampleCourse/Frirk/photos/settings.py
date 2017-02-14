d069cb8f19289aac9fc236ded3a9ad1717b0e7ac
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-02-14 12:45:20 +0100

    moving LICENCES from models to settings

---
+++ python-Django/ExampleCourse/Frirk/photos/settings.py
@@ -0,0 +1,14 @@
+# -*- coding=utf-8 -*-
+
+
+
+COPYRIGHT='RIG'
+COPYLEFT='LEF'
+CREATIVE_COMMONS='CC'
+
+LICENSES=(
+    (COPYRIGHT,'Copyright'),
+    (COPYLEFT,'Copyleft'),
+    (CREATIVE_COMMONS,'Creative Commons')
+)
+

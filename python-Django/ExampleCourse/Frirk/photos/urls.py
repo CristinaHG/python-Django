a1a95303d85a5f3ca7bf86a97b0ca2ef6fe4b86d
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-02-23 18:11:15 +0100

    adding photos urls

---
+++ python-Django/ExampleCourse/Frirk/photos/urls.py
@@ -0,0 +1,39 @@
+# -*- coding=utf-8 -*-
+
+"""frikr URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from photos.api import PhotoViewSet
+from photos.views import HomeView,DetailView,CreateView,PhotoListView,UserPhotosView
+from django.contrib.auth.decorators import login_required
+
+#APIRouter
+router=DefaultRouter()
+router.register(r'api/1.0/photos',PhotoViewSet) #regiter photos url
+
+
+urlpatterns = [
+    #photos urls
+    url(r'^$', HomeView.as_view(),name='photos_home'),
+    url(r'^my-photos/$',login_required(UserPhotosView.as_view()),name='user_photos'),
+    url(r'^photos/$',PhotoListView.as_view(),name='photos_list'),
+    url(r'^photos/(?P<pk>[0-9]+)$',DetailView.as_view(),name='photo_detail'),
+    url(r'^photos/new$',CreateView.as_view(),name='create_photo'),
+
+    #Photos API URLs
+    url(r'' ,include(router.urls)), #incluyo urls de API (photos y users porqe van a través del router)
+]
